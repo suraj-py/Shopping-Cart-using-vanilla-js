b0a3c623b542d800a9193238591744ae5f5fdca5
Suraj Mhatre <surajmcodes@gmail.com>
2023-01-03 21:40:39 +0530

    code updated

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('files added')
+const toggleButton = document.querySelector('.toggle_btn');
+const sideCart = document.querySelector('.side_menu');
+
+toggleButton.addEventListener('click', () => {
+    sideCart.classList.toggle('open');
+})
